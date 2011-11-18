e3c3bf19da9a6f6e30e64c34d69486402c326705
Robert Head <robert.head@gmail.com>
2011-11-18 07:29:58 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virtuous::Application.config.secret_token = '1670c3ccb156dc91a8712c56f6f08dee769c4e68e90f209aec3a569797467641f44e33ee35a4ed2fb1dba4f0f5e483f6255557cb47b0d0a99bcc46db8f7a6117'
